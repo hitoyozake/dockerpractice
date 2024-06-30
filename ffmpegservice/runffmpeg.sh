2cb7ea49caf13f4fc5249179fca15e61a7ba6206
hitoyozake <mikoto_1122@yahoo.co.jp>
2024-06-30 22:04:46 +0900

    update

--- ffmpegservice/runffmpeg.sh
+++ ffmpegservice/runffmpeg.sh
@@ -5,4 +5,21 @@ INPUT=$1
 echo "this is " $1
 cat /home/hosts/sample.txt
 
+pushd /home/hosts/
+
+mp4=`find *.mp4 `
+
+if [ $? = 0 ]; then
+    echo "-----------------"
+    echo $mp4
+    echo "-----------------"
+
+    ffmpeg -y -i $mp4 -ab 256k output.mp3
+    ls *.mp3
+else
+    echo "NO mp4"
+fi
+
+popd
+
 # ffmpeg -i "$1" -acodec libmp3lame -ab 256k "$2" 
